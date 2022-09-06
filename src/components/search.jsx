ad5638fd7c4f4f2cbdadb1370ffa2bf6cab89354
iceddrop <93996532+iceddrop@users.noreply.github.com>
2022-09-06 11:27:20 +0100

    modified search component

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 export default function Search(props){
-
+    let timer = setTimeout(function() { refreshPage(); }, 30000);
+  function refreshPage() {
+    window.location.reload(false);
+  }
     return(
       <div  id="search-card"class="card  bg-dark me-3 text-white" >
         <img src={`https://image.tmdb.org/t/p/w500${props.image}`} className="card-img-top" alt="movie-image"/>
         <div class="card-body">
-          <Link className='show-search-btn' key={props.id} to={`/movieOverview/${props.id}`}>{props.name}</Link>
+          <Link onClick={timer} className='show-search-btn' key={props.id} to={`/movieOverview/${props.id}`}>{props.name}</Link>
         </div>
      </div>    
     )
